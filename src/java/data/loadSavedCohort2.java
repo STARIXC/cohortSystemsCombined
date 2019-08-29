58c003fc7d044aba49c92299116ad12af01547bd
Chris Erricks <starixc@gmail.com>
2019-08-29 08:19:30 +0300

    Periodical Commits

--- src/java/data/loadSavedCohort2.java
+++ src/java/data/loadSavedCohort2.java
@@ -24,126 +24,126 @@ import org.json.JSONObject;
  */
 public class loadSavedCohort2 extends HttpServlet {
 
-    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-          String mflcode="";
-          String yr="";
-          String mn="";
-          String id="";
-          String ct="";
-          String cm="";
-          String fc="";
-          int year=0;
-          cm=request.getParameter("cm");
-          ct=request.getParameter("ct");
-          mn=request.getParameter("mn");
-          yr=request.getParameter("yr");
-          fc=request.getParameter("fc");
-        
-          mflcode=fc.split("_")[0];
-            
-          loadcohort lc= new loadcohort();
-          
-          
-            
-            JSONArray ar= new JSONArray();
-            
-            String tablename="";
-            
-               if(ct.equals("art")){
-                   
-               tablename="art_cohort";
-               
-               }
-            else {
-                   
-               tablename="pmtct_cohort";
-               
-               }
-            
-       
-          
-        // id=year+"_"+newmn+"_"+subpartnerID;//*******************************
-          
-            String sql="";
+            String mflcode = "";
+            String yr = "";
+            String mn = "";
+            String id = "";
+            String ct = "";
+            String cm = "";
+            String fc = "";
+            int year = 0;
+            cm = request.getParameter("cm");
+            ct = request.getParameter("ct");
+            mn = request.getParameter("mn");
+            yr = request.getParameter("yr");
+            fc = request.getParameter("fc");
+
+            mflcode = fc.split("_")[0];
+
+            loadcohort lc = new loadcohort();
+
+            JSONArray ar = new JSONArray();
+
+            String tablename = "";
+            if (ct.equals("art")) {
+                tablename = "art_cohort";
+            } else if (ct.equals("pmtct")) {
+                tablename = "pmtct_cohort";
+            } else if (ct.equals("stf")) {
+                tablename = "stf_cohort";
+            } else {
+                tablename = "new_defaulter_cohort";
+            }
+//code to be executed if all the conditions are false  
+
+            // id=year+"_"+newmn+"_"+subpartnerID;//*******************************
+            String sql = "";
             //out.println("</html>");
-            
-            dbConn conn= new dbConn();
-            
-            if(!mn.equals("")&& !yr.equals("") &&! fc.equals("")){
-            //get and return data
-            if(ct.equalsIgnoreCase("art")){
-            
-            sql="select id, indicator, IFNULL(adult_"+cm+",'0') as adult_"+cm+" , IFNULL(child_"+cm+",'0') as child_"+cm+", IFNULL(tl_"+cm+",'0') as tl_"+cm+"  ";
+
+            dbConn conn = new dbConn();
+
+            if (!mn.equals("") && !yr.equals("") && !fc.equals("")) {
+                //get and return data
+                if (ct.equalsIgnoreCase("art")) {
+
+                    sql = "select id, indicator, IFNULL(adult_" + cm + ",'0') as adult_" + cm + " , IFNULL(child_" + cm + ",'0') as child_" + cm + ", IFNULL(tl_" + cm + ",'0') as tl_" + cm + "  ";
+                } else if (ct.equalsIgnoreCase("pmtct")) {
+                    sql = "select  id, indicator ,IFNULL(kp_" + cm + ",'') as kp_" + cm + " , IFNULL(np_" + cm + ",'') as np_" + cm + " ,IFNULL(tl_" + cm + ",'') as tl_" + cm + " ";
+                } else if (ct.equalsIgnoreCase("stf")) {
+
+                    sql = "select id, indicator,IFNULL(child_"+cm+",'0') as child_"+cm+" , IFNULL(adult_" + cm + ",'0') as adult_" + cm + " , IFNULL(ayp_" + cm + ",'0') as ayp_" + cm + ", IFNULL(tl_" + cm + ",'0') as tl_" + cm + "  ";
+
+                } else {
+                    sql = "select  id, indicator ,IFNULL(np_" + cm + ",'') as np_" + cm + " , IFNULL(def_" + cm + ",'') as def_" + cm + " ,IFNULL(tl_" + cm + ",'') as tl_" + cm + " ";
+
+                }
+
+                sql += " from " + tablename + " where mflcode='" + mflcode + "' and yearmonth='" + yr + mn + "' and (indicator!='12' ) and (indicator!='24' ) order by indicator ";
+
+                conn.rs = conn.st0.executeQuery(sql);
+                System.out.println("" + sql);
+
+                while (conn.rs.next()) {
+
+                    JSONObject jo = new JSONObject();
+                    jo.put("indicator", conn.rs.getString(2));
+                    //always show 0's for LTFU 3m
+                    if (cm.equalsIgnoreCase("3m") && (conn.rs.getString(2).equals("6") || conn.rs.getString(2).equals("18"))) {
+                        jo.put("val1", 0);
+                        jo.put("val2", 0);
+                        jo.put("val3", 0);
+                        System.out.println("Indicator " + conn.rs.getInt(2) + " no LTFU");
+                    } //no values for vl done and suppressed Np 3m
+                    else if (ct.equalsIgnoreCase("pmtct") && cm.equalsIgnoreCase("3m") && (conn.rs.getString(2).equals("10") || conn.rs.getString(2).equals("11"))) {
+                        jo.put("val1", conn.rs.getString(3));
+                        jo.put("val2", 0);
+                        jo.put("val3", conn.rs.getString(5));
+                        System.out.println("Indicator " + conn.rs.getString(2) + " if 2------");
+                    } //no values for vl done and suppressed
+                    else if (ct.equalsIgnoreCase("pmtct") && cm.equalsIgnoreCase("3m") && (conn.rs.getString(2).equals("10") || conn.rs.getString(2).equals("11"))) {
+                        jo.put("val1", conn.rs.getString(3));
+                        jo.put("val2", 0);
+                        jo.put("val3", conn.rs.getString(5));
+                        System.out.println("Indicator " + conn.rs.getString(2) + " if 2------");
+                    } //no values for vl done and suppressed
+                    else if (cm.equals("9m") && (conn.rs.getString(2).equals("10") || conn.rs.getString(2).equals("11"))) {
+                        jo.put("val1", 0);
+                        jo.put("val2", 0);
+                        jo.put("val3", 0);
+                        System.out.println("Indicator " + conn.rs.getString(2) + " if 3------");
+                    } else {
+                        jo.put("val1", conn.rs.getString(3));
+                        jo.put("val2", conn.rs.getString(4));
+                        jo.put("val3", conn.rs.getString(5));
+
+                    }
+
+                    ar.put(jo);
+
+                }
+
             }
-            
-            
-            else {
-            
-            sql="select  id, indicator ,IFNULL(kp_"+cm+",'') as kp_"+cm+" , IFNULL(np_"+cm+",'') as np_"+cm+" ,IFNULL(tl_"+cm+",'') as tl_"+cm+" ";
-            
+            if (conn.rs != null) {
+                conn.rs.close();
             }
-            
-            sql+=" from "+tablename+" where mflcode='"+mflcode+"' and yearmonth='"+yr+mn+"' and (indicator!='12' ) and (indicator!='24' ) order by indicator ";
-            
-            
-            conn.rs=conn.st0.executeQuery(sql);
-                System.out.println(""+sql);
-            
-            while (conn.rs.next()){
-                
-             JSONObject jo= new JSONObject(); 
-        jo.put("indicator", conn.rs.getString(2));
-        //always show 0's for LTFU 3m
-             if( cm.equalsIgnoreCase("3m") && (conn.rs.getString(2).equals("6") || conn.rs.getString(2).equals("18") )){
-        jo.put("val1", 0);
-        jo.put("val2", 0);
-        jo.put("val3", 0);
-        System.out.println("Indicator "+conn.rs.getInt(2)+" no LTFU");
-             }
-              //no values for vl done and suppressed Np 3m
-             else if(ct.equalsIgnoreCase("pmtct") && cm.equalsIgnoreCase("3m") && (conn.rs.getString(2).equals("10")|| conn.rs.getString(2).equals("11") ) ){
-        jo.put("val1",  conn.rs.getString(3));
-        jo.put("val2", 0);
-        jo.put("val3", conn.rs.getString(5));
-        System.out.println("Indicator "+conn.rs.getString(2)+" if 2------");
-             }
-             //no values for vl done and suppressed
-             else if(  cm.equals("9m") && (conn.rs.getString(2).equals("10")|| conn.rs.getString(2).equals("11") ) ){
-        jo.put("val1", 0);
-        jo.put("val2", 0);
-        jo.put("val3", 0);
-        System.out.println("Indicator "+conn.rs.getString(2)+" if 3------");
-             }
-             else {
-        jo.put("val1", conn.rs.getString(3));
-        jo.put("val2", conn.rs.getString(4));
-        jo.put("val3", conn.rs.getString(5));
-             
-             }
-         
-        ar.put(jo);
-            
+            if (conn.st0 != null) {
+                conn.st0.close();
             }
-            
+            if (conn.conn != null) {
+                conn.conn.close();
             }
-            if(conn.rs!=null){conn.rs.close();}
-            if(conn.st0!=null){conn.st0.close();}
-            if(conn.conn!=null){conn.conn.close();}
-           
-            
-             out.println(ar);
-            
+
+            out.println(ar);
+
         }
-        
-      
+
     }
 
-    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -154,7 +154,6 @@ public class loadSavedCohort2 extends HttpServlet {
         }
     }
 
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -165,7 +164,6 @@ public class loadSavedCohort2 extends HttpServlet {
         }
     }
 
-    
     @Override
     public String getServletInfo() {
         return "Short description";
